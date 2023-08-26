be04403aba3e4178019ffede41a7e9b1ec1bb850
SampleSpaceDev <sam@samplespace.dev>
2023-08-26 17:58:03 +0100

    Create bot

---
+++ MovieNightBot/Program.cs
@@ -0,0 +1,29 @@
+﻿using DSharpPlus;
+using DSharpPlus.SlashCommands;
+using Microsoft.Extensions.Logging;
+
+namespace MovieNightBot;
+
+internal static class Program
+{
+    private static Config? Options { get; set; }
+
+    private static async Task Main()
+    {
+        Options = (await Config.Load())!;
+
+        var discord = new DiscordClient(new DiscordConfiguration
+        {
+            Token = Options.Token,
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.AllUnprivileged
+        });
+
+        var slashCommands = discord.UseSlashCommands();
+        discord.Logger.LogInformation("[SLASH COMMAND] Registering slash commands.");
+        slashCommands.RegisterCommands(typeof(Program).Assembly, Options.GuildId);
+        
+        await discord.ConnectAsync();
+        await Task.Delay(-1);
+    }
+}
