1daac7a5e63921f3bcc584e88c191df543047c96
SampleSpaceDev <sam@samplespace.dev>
2023-08-26 17:58:13 +0100

    Create config loader and class

---
+++ MovieNightBot/Config.cs
@@ -0,0 +1,34 @@
+using System.Text.Json;
+
+namespace MovieNightBot;
+
+public class Config
+{
+    public string Token { get; set; }
+    public ulong GuildId { get; set; }
+    
+    internal static async Task<Config?> Load()
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        var json = await File.ReadAllTextAsync("config.json");
+        var config = JsonSerializer.Deserialize<Config>(json, options);
+
+        return config;
+    }
+
+    internal static async Task Save(Config config)
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+
+        var json = JsonSerializer.Serialize(config, options);
+        await File.WriteAllTextAsync("config.json", json);
+    }
+}
